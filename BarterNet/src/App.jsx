ce6bc47243386e2cfd4d652d732069eda2141b00
Sebastian <sebastian.glonek86@icloud.com>
2024-05-27 17:14:20 +0200

    JFDZR13YT-3: Create basic route components and configure routing for them.
    - Layout
    - Header
    - Footer
    - PublicHomePage
    - Login
    - Register
    - PrivateRoute
    - UserHomePage

--- BarterNet/src/App.jsx
+++ BarterNet/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout/Layout'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import UserHomePage from './components/UserHomePage/UserHomePage'
+import PrivateRoute from './utils/PrivateRoute'
 
 function App() {
 
-
   return (
-    <>
-    </>
+  <Routes>
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element={<PublicHomePage />}/>
+      <Route path='login' element={<Login />}/>
+      <Route path='register' element={<Register />} />
+      <Route path='/' element={<PrivateRoute />}>
+        <Route path='/' element={<UserHomePage />}/>
+        {/* Tutaj dodamy kolejne routy dla zalogowanych userów  */}
+      </Route>
+    </Route>
+  </Routes>
+    
   )
 }
 
