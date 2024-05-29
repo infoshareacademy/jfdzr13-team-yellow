b8cae330ff3c33bcfeefd7f40ba39e2951d4e848
Magdalena Głowienka <magdalena.glowienka92@gmail.com>
2024-05-29 23:14:41 +0200

    JFDZR13YT-2-added a registration form

--- BarterNet/src/components/Register/Register.jsx
+++ BarterNet/src/components/Register/Register.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./Register.module.css";
 
-const Register = () => {
+function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    // tutaj
+    e.preventDefault();
+    // i tutaj
+   
+    console.log(formData);
+    
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div className={styles.registerContainer}>
+      <form onSubmit={handleSubmit} className={styles.registerForm}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="password">Hasło:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="confirmPassword">Potwierdź hasło:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit" className={styles.submitButton}>
+          Zarejestruj się
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
