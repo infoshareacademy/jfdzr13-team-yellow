c695375ec74173d24a0b9a5b72a976eed368ddee
Joanna Taratuta <joannataratuta@gmail.com>
2024-06-02 08:40:12 +0200

    Add styling to home page

--- BarterNet/src/components/PublicHomePage/PublicHomePage.jsx
+++ BarterNet/src/components/PublicHomePage/PublicHomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./PublicHomePage.module.css";
 
 const PublicHomePage = () => {
   return (
@@ -6,60 +7,62 @@ const PublicHomePage = () => {
       <div>PublicHomePage</div>
 
       <div>
-        <section className="hero">
+        <section className={styles.hero}>
           <h1>Wymień się!</h1>
           <p>
             Znajdź najlepsze oferty i wymieniaj się usługami, na te, których
             potrzebujesz. Dołącz do nas i zacznij korzystać z możliwości, jakie
             daje nowoczesny barter.
           </p>
-          <button>Zacznij teraz</button>
+          <div className={styles.heroButtonWrapper}>
+            <div>
+              <button className={styles.heroButton}>Zacznij teraz</button>
+            </div>
+          </div>
+          <div className={styles.herohalfcircle1}></div>
+          <div className={styles.herohalfcircle2}></div>
         </section>
 
         {/* Sekcja 2 */}
-        <section className="buttons-section">
-          <button>Najpopularniejsze oferty</button>
-          <button>Poznaj korzyści barteru</button>
-          <button>Pierwsze kroki</button>
+        <section className={styles.buttonsSection}>
+          <div className={styles.buttonscontainer}>
+            <button className={styles.button}>Najpopularniejsze oferty</button>
+            <button className={styles.button}>Poznaj korzyści barteru</button>
+            <button className={styles.button}>Pierwsze kroki</button>
+          </div>
         </section>
 
         {/* Sekcja 3 */}
-        <section className="articles">
-          <article>
-            <img
-              src="https://t4.ftcdn.net/jpg/05/34/48/37/360_F_534483775_2hBgOxryd3El6t3tKOtbcM95Yq3OmTGG.jpg"
-              alt=""
-            />
-            <h2>Nauka angielskiego online</h2>
-            <p>
-              Chętnie naucze Cię języka angielskiego. Przez 20 lat mieszkałam w
-              Angli i mam doświadczenie w...
-            </p>
-            <button>Wymień się</button>
+        <section className={styles.articles}>
+          <article className={styles.article}>
+            <div className={styles.article__container}>
+              <h2>Nauka angielskiego online</h2>
+              <p>
+                Chętnie naucze Cię języka angielskiego. Przez 20 lat mieszkałam
+                w Angli i mam doświadczenie w...
+              </p>
+              <button className={styles.articleButton}>Wymień się</button>
+            </div>
           </article>
-          <article>
-            <img
-              src="https://t4.ftcdn.net/jpg/05/34/48/37/360_F_534483775_2hBgOxryd3El6t3tKOtbcM95Yq3OmTGG.jpg"
-              alt=""
-            />
-            <h2>Wyprowadzanie psa</h2>
-            <p>
-              Nie masz czasu na poranne spacery ze swoim pupuliem, chętnie Ci w
-              tym pomogę, bo...
-            </p>
-            <button>Wymień się</button>
+          <article className={styles.article}>
+            <div className={styles.article__container}>
+              <h2>Wyprowadzanie psa</h2>
+              <p>
+                Nie masz czasu na poranne spacery ze swoim pupuliem, chętnie Ci
+                w tym pomogę, bo...
+              </p>
+              <button className={styles.articleButton}>Wymień się</button>
+            </div>
           </article>
-          <article>
-            <img
-              src="https://t4.ftcdn.net/jpg/05/34/48/37/360_F_534483775_2hBgOxryd3El6t3tKOtbcM95Yq3OmTGG.jpg"
-              alt=""
-            />
-            <h2>Nauka gry na gitarze</h2>
-            <p>
-              Zawsze marzyłeś o tym, żeby grać na gitarze, ale rodzice zapisali
-              Cię na karate, skorzystaj z...
-            </p>
-            <button>Wymień się</button>
+          <article className={styles.article}>
+            <div className={styles.article__container}>
+              <h2>Nauka gry na gitarze</h2>
+              <p>
+                Zawsze marzyłeś o tym, żeby grać na gitarze, ale rodzice
+                zapisali Cię na karate, skorzystaj z...
+              </p>
+              <button className={styles.articleButton}>Wymień się</button>
+            </div>
           </article>
         </section>
       </div>
