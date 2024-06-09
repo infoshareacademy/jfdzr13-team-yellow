122625925cc965f558dcbaa13995f193dfa5d241
Joanna Taratuta <joannataratuta@gmail.com>
2024-06-09 11:37:40 +0200

    Update ListingItem component to include a Link component from React Router for the article button, directing to the appropriate article route.

--- BarterNet/src/components/ListingItem/ListingItem.jsx
+++ BarterNet/src/components/ListingItem/ListingItem.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 
+import { useAuth } from "../../contex/AuthProvider";
+
 import styles from "./ListingItem.module.css";
 
-const ListingItem = ({ title, content }) => {
+const ListingItem = ({ index, title, content }) => {
+  const { currentUser } = useAuth();
+
   return (
     <div className={styles.article}>
       <div className={styles.article__container}>
         <h2>{title}</h2>
         <p>{content}</p>
-        <button className={styles.articleButton}>Wymień się</button>
+        {(() => {
+          if (currentUser) {
+            return (
+              <>
+                <a href={`/article${index}`}>
+                  <button className={styles.articleButton}>Wymień się</button>
+                </a>
+              </>
+            );
+          } else {
+            return (
+              <>
+                <a href="/register">
+                  <button className={styles.articleButton}>Wymień się</button>
+                </a>
+              </>
+            );
+          }
+        })()}
       </div>
     </div>
   );
--- BarterNet/src/components/PublicHomePage/PublicHomePage.jsx
+++ BarterNet/src/components/PublicHomePage/PublicHomePage.jsx
@@ -7,56 +7,67 @@ import HeroBackground from "./assets/hero-background.png";
 
 const articles = [
   {
+    id: 1,
     title: "Nauka angielskiego online",
     content:
       "Chętnie naucze Cię języka angielskiego. Przez 20 lat mieszkałam w Angli i mam doświadczenie w...",
   },
   {
+    id: 2,
     title: "Wyprowadzanie psa",
     content:
       "Nie masz czasu na poranne spacery ze swoim pupuliem, chętnie Ci w tym pomogę, bo...",
   },
   {
+    id: 3,
     title: "Nauka gry na gitarze",
     content:
       "Zawsze marzyłeś o tym, żeby grać na gitarze, ale rodzice zapisali Cię na karate, skorzystaj z...",
   },
   {
+    id: 4,
     title: "Korepetycje z matematyki",
     content:
       "Potrzebujesz pomocy w zrozumieniu matematyki? Jestem do Twojej dyspozycji. Pomożemy Ci przejść przez zagadnienia od podstaw do bardziej zaawansowanych.",
   },
   {
+    id: 5,
     title: "Kurs gotowania wegetariańskiego",
     content:
       "Chcesz nauczyć się przygotowywać pyszne wegetariańskie potrawy? Zapraszam na kurs gotowania, gdzie nauczymy się przyrządzać zdrowe i smaczne dania bez mięsa.",
   },
   {
+    id: 6,
     title: "Usługi opiekuńcze dla osób starszych",
     content:
       "Potrzebujesz wsparcia w opiece nad osobą starszą? Oferuję profesjonalne usługi opiekuńcze, w tym pomoc w codziennych czynnościach oraz towarzyszenie na spacerach i wizytach lekarskich.",
   },
   {
+    id: 7,
     title: "Zajęcia jogi dla początkujących",
     content:
       "Chcesz zacząć praktykować jogę, ale nie wiesz od czego zacząć? Zapraszam na zajęcia dla początkujących, gdzie nauczysz się podstawowych pozycji i technik oddechowych.",
   },
   {
+    id: 8,
     title: "Pranie i prasowanie",
     content:
       "Brakuje Ci czasu na pranie i prasowanie? Oferuję profesjonalne usługi prania oraz prasowania, abyś mógł cieszyć się czystą i wyprasowaną odzieżą bez trudu.",
   },
   {
+    id: 9,
     title: "Remontowanie mebli",
     content:
       "Twoje meble potrzebują odświeżenia? Przynieś je do mnie, a z przyjemnością pomaluję, wypoleruję lub naprawię uszkodzenia, abyś mógł cieszyć się pięknym wyglądem swojego mieszkania.",
   },
   {
+    id: 10,
     title: "Instrukcje z zakresu pierwszej pomocy",
     content:
       "Chcesz nauczyć się udzielać pierwszej pomocy w nagłych wypadkach? Oferuję instrukcje z zakresu udzielania pomocy przedmedycznej, abyś mógł być gotowy na każdą sytuację.",
   },
   {
+    id: 11,
     title: "Nauka tańca towarzyskiego",
     content:
       "Chcesz nauczyć się tańczyć w stylu latynoamerykańskim lub standardowym? Zapraszam na zajęcia z tańca towarzyskiego, gdzie nauczysz się eleganckich i efektownych figur pod okiem doświadczonych instruktorów.",
@@ -106,6 +117,7 @@ const PublicHomePage = () => {
             {randomArticles.map((article, index) => (
               <ListingItem
                 key={index}
+                index={article.id}
                 title={article.title}
                 content={article.content}
               />
