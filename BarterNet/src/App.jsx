7cc4027f12d2840e937b2fb646e68c49767120ce
Sebastian <sebastian.glonek86@icloud.com>
2024-06-15 13:14:26 +0200

    JFDZR13YT-13: Add route for new SearchPage component

--- BarterNet/src/App.jsx
+++ BarterNet/src/App.jsx
@@ -2,11 +2,12 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout/Layout'
 import Login from './components/Login/Login'
+import MyAccount from './components/MyAccount/MyAccount'
+import PasswordsReset from './components/PasswordsReset/PasswordsReset'
 import Register from './components/Register/Register'
 import UserHomePage from './components/UserHomePage/UserHomePage'
-import PasswordsReset from './components/PasswordsReset/PasswordsReset';
-import MyAccount from './components/MyAccount/MyAccount';
 
+import SearchPage from './components/SearchPage/SearchPage'
 import PrivateRoute from './utils/PrivateRoute'
 
 
@@ -20,7 +21,8 @@ function App() {
       <Route path='passwordReset' element={<PasswordsReset />} /> {/* Trasa do resetowania hasła */}
       <Route path='/' element={<PrivateRoute />}>
         <Route index element={<UserHomePage />}/>
-        <Route path='myAccount' element={<MyAccount />} /> {/* Nowa ścieżka */}
+        <Route path='myAccount' element={<MyAccount />} />
+        <Route path='searchpage' element={<SearchPage />} />
         {/* Tutaj dodamy kolejne routy dla zalogowanych userów  */}
       </Route>
       <Route path='*' element={<Navigate to={'/'} />} /> 
