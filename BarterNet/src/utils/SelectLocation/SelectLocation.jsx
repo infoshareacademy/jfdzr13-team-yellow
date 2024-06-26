85686c2367029e0592ceed077b2a3dc6f9623b13
Sebastian <sebastian.glonek86@icloud.com>
2024-06-26 09:27:42 +0200

    JFDZR13YT-13: Added category icons

--- BarterNet/src/utils/SelectLocation/SelectLocation.jsx
+++ BarterNet/src/utils/SelectLocation/SelectLocation.jsx
@@ -3,25 +3,37 @@ import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { db } from '../../config/firebase';
 import styles from './SelectLocation.module.css'
-
+import {locations} from '../locationsList'
 const SelectLocation = ({placeholder, onChange, name, value}) => {
   const [options, setOptions] = useState([]);
 
-  useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'locations'), (querySnapshot) => {
-      const optionsData = querySnapshot.docs.map((doc) => {
-        const data = doc.data();
-        return { value: `${data.city}, ${data.voivodeship}`, label: `${data.city}, ${data.voivodeship}` };
-      });
-      setOptions(optionsData);
-    }, (error) => {
-      console.error('Error fetching data:', error);
+//   useEffect(() => {
+//     const unsubscribe = onSnapshot(collection(db, 'locations'), (querySnapshot) => {
+//       const optionsData = querySnapshot.docs.map((doc) => {
+//         const data = doc.data();
+//         return { value: `${data.city}, ${data.voivodeship}`, label: `${data.city}, ${data.voivodeship}` };
+//       });
+//       setOptions(optionsData);
+//     }, (error) => {
+//       console.error('Error fetching data:', error);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   return <Select className={styles.select} options={options} placeholder={placeholder} onChange={onChange} name={name} value={value}/>;
+// };
+
+useEffect(() => {
+    const optionsData = locations.map((doc) => {
+     
+      return { value: `${doc.city}, ${doc.voivodeship}`, label: `${doc.city}, ${doc.voivodeship}` };
     });
+    setOptions(optionsData);
+  
+}, []);
 
-    return () => unsubscribe();
-  }, []);
-
-  return <Select className={styles.select} options={options} placeholder={placeholder} onChange={onChange} name={name} value={value}/>;
+return <Select className={styles.select} options={options} placeholder={placeholder} onChange={onChange} name={name} value={value}/>;
 };
 
 export default SelectLocation;
