a1d62290733108f3ee49f8722f65135509613c7f
Sebastian <sebastian.glonek86@icloud.com>
2024-06-28 17:18:44 +0200

    JFDZR13YT-13: Complete SearchPage component.
    - Refactor logic for improved functionality
    - Add CSS styles to component.
    - Restore reset css and define new global style in index.css.
    - Add default image.

--- BarterNet/src/components/SearchPage/SearchPage.jsx
+++ BarterNet/src/components/SearchPage/SearchPage.jsx
@@ -2,75 +2,13 @@ import React, { useEffect, useRef, useState } from 'react'
 import SelectLocation from '../../utils/SelectLocation/SelectLocation'
 import styles from './SearchPage.module.css'
 import ListingItem from '../ListingItem/ListingItem'
+import categories from '../../utils/categoriesList'
+import toast, { Toaster } from 'react-hot-toast'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+import { db } from '../../config/firebase'
 
 const SearchPage = () => {
-    const [fetchedListings, setFetchedListings] = useState([
-      {
-        title: "Pomoc w projektowaniu ogrodu",
-        description: "Oferuję pomoc w zaprojektowaniu i założeniu ogrodu. Posiadam doświadczenie w projektowaniu przestrzeni zielonych.",
-        foto: ['https://as1.ftcdn.net/v2/jpg/05/49/73/80/1000_F_549738015_khpYAvf2Gtv1bLn0Mmd8VmYjz6tRhOdw.jpg'],
-        location: "Szczecin, Zachodniopomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user1Id"
-      },
-      {
-        title: "Pomoc w projektowaniu ogrodu",
-        description: "Oferuję pomoc w zaprojektowaniu i założeniu ogrodu. Posiadam doświadczenie w projektowaniu przestrzeni zielonych.",
-        foto: ['https://as1.ftcdn.net/v2/jpg/05/49/73/80/1000_F_549738015_khpYAvf2Gtv1bLn0Mmd8VmYjz6tRhOdw.jpg'],
-        location: "Szczecin, Zachodniopomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user1Id"
-      },
-      {
-        title: "Pomoc w projektowaniu ogrodu",
-        description: "Oferuję pomoc w zaprojektowaniu i założeniu ogrodu. Posiadam doświadczenie w projektowaniu przestrzeni zielonych.",
-        foto: ['https://as1.ftcdn.net/v2/jpg/05/49/73/80/1000_F_549738015_khpYAvf2Gtv1bLn0Mmd8VmYjz6tRhOdw.jpg'],
-        location: "Szczecin, Zachodniopomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user1Id"
-      },
-      {
-        title: "Pomoc w projektowaniu ogrodu",
-        description: "Oferuję pomoc w zaprojektowaniu i założeniu ogrodu. Posiadam doświadczenie w projektowaniu przestrzeni zielonych.",
-        foto: ['https://as1.ftcdn.net/v2/jpg/05/49/73/80/1000_F_549738015_khpYAvf2Gtv1bLn0Mmd8VmYjz6tRhOdw.jpg'],
-        location: "Szczecin, Zachodniopomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user1Id"
-      },
-      {
-        title: "Pomoc w renowacji mebli",
-        description: "Oferuję pomoc w renowacji mebli drewnianych. Mam doświadczenie w malowaniu i renowacji mebli.",
-        foto: [],
-        location: "Szczecin, Zachodniopomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user1Id"
-      },
-      {
-        title: "Warsztaty malowania",
-        description: "Oferuję warsztaty malowania i rysunku dla dzieci i dorosłych.",
-        foto: [],
-        location: "Gdańsk, Pomorskie",
-        category: "Hobby i rozrywka",
-        type: "offer",
-        userId: "user4Id"
-      },
-      {
-        title: "Opieka nad dziećmi",
-        description: "Oferuję opiekę nad dziećmi w wieku przedszkolnym. Jestem odpowiedzialna i kreatywna.",
-        foto: [],
-        location: "Gdańsk, Pomorskie",
-        category: "Dom i ogród",
-        type: "offer",
-        userId: "user4Id"
-      },
-    ]) /* Lista wszystkich ogłoszeń, wszystkich userów ograniczona do typu ofert: "offer" lub "search"  */
-
-
+    const [fetchedListings, setFetchedListings] = useState([]) /* Lista wszystkich ogłoszeń, wszystkich userów ograniczona do typu ofert: "offer" lub "search"  */
     const [filteredListings, setFilteredListings] = useState([])
     const [isButtonDisabled, setIsButtonDisabled] = useState(true)
 const [searchParams, setSearchParams] = useState({
@@ -81,50 +19,34 @@ const [searchParams, setSearchParams] = useState({
 })
 const [isSomeParams, setIsSomeParams] =useState(false)
 
-
-const categories=[
-    "Dom i ogród",
-    "Edukacja",
-    "Transport i logistyka",
-    "Sport i rekreacja",
-    "Hobby i rozrywka",
-    "Zwierzęta",
-    "Technologia i IT",
-    "Zdrowie i uroda",
-    "Inne"
-]
-
 const formRef = useRef(null)
 
-// useEffect(() => {
-//   const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
-//       const listings = [];
-//       snapshot.forEach((user) => {
-//         const userId = user.id;
-//         const userListingsRef = collection(db, 'users', userId, 'listings');
-//         const filteredListingsQuery = query(userListingsRef, where('type', '==', searchParams.type));
+useEffect(() => {
+  const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
+      const listings = [];
+      snapshot.forEach((user) => {
+        const userId = user.id;
+        const userListingsRef = collection(db, 'users', userId, 'listings');
+        const filteredListingsQuery = query(userListingsRef, where('type', '==', searchParams.type));
 
-//         onSnapshot(filteredListingsQuery, (querySnapshot) => {
-//           querySnapshot.forEach((listingDocument) => {
-//             listings.push({
-//               listingId: listingDocument.id,
-//               ...listingDocument.data()
-//             });
-//           });
-//         });
-//       });
-//       setFetchedListings(listings);
-//     },
-//     (error) => {
-//       console.error('Error fetching data:', error);
-//     }
-//   );
-
-//   return () => unsubscribe();
-// }, [searchParams.type]);
-
-
-console.log(`1 filteredListings: ${JSON.stringify(fetchedListings)}`)
+        onSnapshot(filteredListingsQuery, (querySnapshot) => {
+          querySnapshot.forEach((listingDocument) => {
+            listings.push({
+              listingId: listingDocument.id,
+              ...listingDocument.data()
+            });
+          });
+        });
+      });
+      setFetchedListings(listings);
+    },
+    (error) => {
+      console.error('Error fetching data:', error);
+    }
+  );
+console.log(fetchedListings)
+  return () => unsubscribe();
+}, [searchParams.type]);
 
     useEffect(() => {
         (searchParams.location || searchParams.category || searchParams.keyword) ? setIsSomeParams(true)  : setIsSomeParams(false) && setFilteredListings([])
@@ -141,7 +63,6 @@ return {
     type: type
 }
     })
-
 
 }
 const setCategory = (category) => {
@@ -167,7 +88,7 @@ keyword: e.target.value
       }))
     }
   };
-
+  
 
 const clearFilter = (key) => {
     setSearchParams((prev) => 
@@ -181,8 +102,10 @@ const clearFilter = (key) => {
     ({
       ...prev,
       [key]: null,
-    }));
+    }))
+    ;
     setFilteredListings([])
+    formRef.current.reset()
   };
 
   const setLocation = (selectedOption) => {
@@ -192,18 +115,13 @@ const clearFilter = (key) => {
     }));
   };
     
-   
 const handleSubmit = (e) => {
     e.preventDefault();
     let finalyList = [];
     fetchedListings.forEach((listing) =>  {
             finalyList.push(listing);
         });
-    
-
-    // Przefiltruj listę zgodnie z warunkami
     const result = finalyList.filter((listing) => {
-        // Upewnij się, że sprawdzamy każde z warunków osobno
         const matchesLocation = !searchParams.location || listing.location === searchParams.location;
         const matchesCategory = !searchParams.category || listing.category === searchParams.category;
         const matchesKeyword = !searchParams.keyword || 
@@ -213,76 +131,119 @@ const handleSubmit = (e) => {
         // Listing zostaje jeśli spełnia wszystkie warunki
         return matchesLocation && matchesCategory && matchesKeyword;
     });
-
-    setFilteredListings(result);
-    console.log(result);
+    setFilteredListings(result)
     formRef.current.reset();
+    if (result.length === 0) {
+      toast("W tej chwili nie mamy żadnych ofert spełniających Twoje kryteria wyszukiwania.", { duration: 3000});
+    }  
 };
 
-
-
   return (
-    <>
-   <section className={styles.search}>
+<>
+<Toaster
+position='bottom-center'
+toastOptions={{
+  style: {
+    padding: '30px',
+    fontSize: '32px',
+  },
+}}
+/>
+  <section className={styles.search}>
     <h1>Wyszukaj ogłoszenie</h1>
-    <form id={'search'} className={styles.searchForm} onSubmit={handleSubmit} onKeyDown={handleKeyDown}  ref={formRef}>
-        {isSomeParams && (
-            <div className={styles.searchForm__filter}>
-                 {(searchParams.keyword) && ( 
-                    <button type='button' onClick={() => clearFilter('keyWord')} >{searchParams.keyword}</button> 
-                    )}
-                 {(searchParams.category) && (
-                    <button type='button' onClick={() => clearFilter('category')} >{searchParams.category}</button>
-                 )}
-                 {(searchParams.location) && (
-                    <button type='button' onClick={() => clearFilter('location')} >{searchParams.location}</button>
-                 )}
-                 {(<button type='button' onClick={() => clearFilter('reset')} >Wyczyść wszystkie filtry</button>)}
+    <form 
+      id={'search'} 
+      className={styles.searchForm} 
+      onSubmit={handleSubmit} 
+      ref={formRef}>
+        <fieldset className={styles.searchForm__filter}>
+          {(searchParams.keyword) && ( 
+            <button className={styles.searchForm__filterBtn} type='button' onClick={() => clearFilter('keyword')} >{searchParams.keyword}</button> 
+          )}
+          {(searchParams.category) && (
+            <button className={styles.searchForm__filterBtn} type='button' onClick={() => clearFilter('category')} >{searchParams.category}</button>
+          )}
+          {(searchParams.location) && (
+            <button className={styles.searchForm__filterBtn} type='button' onClick={() => clearFilter('location')} >{searchParams.location}</button>
+          )}
+          { isSomeParams && (
+            <button className={`${styles.searchForm__filterBtn}  ${styles.searchForm__filterReset}`} type='button' onClick={() => clearFilter('reset')} >Wyczyść wszystkie filtry</button>)}
                 
-            </div> )}
-        <div className={styles.searchForm__btnWrap}>
-            <button type='button' onClick={() =>setListingType('offer')} className={styles.searchForm__btn}>OFERUJĘ</button>
-            <button type='button' onClick={() =>setListingType('search')} className={styles.searchForm__btn}>POTRZEBUJĘ</button>
-        </div>
-        <div>
-    { !searchParams.keyword && ( <label> Wyszukaj
-        <input className={styles.searchForm__searchField} name='search' type="text" onSubmit={setKeyword} />
-    </label>)}
-    <label> Lokalizacja
-        <SelectLocation 
-        placeholder={"Wybierz lokalizację"}
-        onChange={setLocation}
-        name={'location'}
-        value={searchParams.location ? {value: searchParams.location, label: searchParams.location} : null}
-        />
-    </label>
-    </div>
-    <div className={styles.searchForm__categoryWrap}>
-        <p>Kategoria</p>
-        <div className={styles.searchForm__categoryBtn}>
-        { categories.map((category, index)=> {
-            return <button key={index} onClick={(e) => {
-                e.preventDefault()
-            setCategory(category)}} className={styles.btn}><span>{category}</span></button>
-        }) }
-        </div>
-       
-    </div>
-        <button disabled={isButtonDisabled}>Szukaj</button>
+        </fieldset>
+        <fieldset className={styles.searchForm__typeWrap}>
+            <button 
+              type='button' 
+              onClick={() =>setListingType('offer')} 
+              className={`${styles.searchForm__typeBtn} ${searchParams.type === 'offer' ? styles.searchForm__typeBtnActive : ''}`}
+            >OFERUJĘ</button>
+            <button 
+              type='button' 
+              onClick={() =>setListingType('search')} 
+              className={`${styles.searchForm__typeBtn} ${searchParams.type === 'search' ? styles.searchForm__typeBtnActive : ''}`}
+            >POTRZEBUJĘ</button>
+        </fieldset>
+        <fieldset className={styles.searchForm__textFields}>
+          
+            <label> 
+              <h2>Wyszukaj</h2>
+              <input 
+              onKeyDown={handleKeyDown}  
+                className={styles.searchForm__searchField} 
+                name='search' 
+                type="text" 
+                onSubmit={setKeyword} 
+                placeholder={'Szukaj'}/>
+            </label>
+            <label> 
+              <h2>Lokalizacja</h2>
+              <SelectLocation 
+                placeholder={"Wybierz lokalizację"}
+                onChange={setLocation}
+                name={'location'}
+                value={searchParams.location ? {value: searchParams.location, label: searchParams.location} : null}
+              />
+            </label>
+        </fieldset>
+        <fieldset className={styles.searchForm__categoryWrap}>
+          <h2>Kategoria</h2>
+          <div className={styles.searchForm__categoryButtons}>
+            {categories.map((obj, index)=> (
+              <button 
+                key={index} 
+                className={`${styles.searchForm__categoryBtn} ${searchParams.category === obj.category ? styles.searchForm__categoryBtnActive : ''}`} 
+                style={{backgroundImage: `url("${obj.icon}")`}}
+                onClick={(e) => {
+                  e.preventDefault()
+                  setCategory(obj.category)}} 
+              ><span>{obj.category}</span></button>
+            )) }
+          </div>
+        </fieldset>
+        <button 
+          className={`${styles.searchForm__submitBtn} ${isButtonDisabled ? styles.searchForm__submitBtnDisabled : '' }`}
+          disabled={isButtonDisabled}
+        ><h2>Szukaj</h2></button>
     </form>
-    </section>
-    <section className={styles.listings}>
-        {filteredListings && filteredListings.map((obj, index) => (
+  </section>
+  {filteredListings.length > 0 && (
+  <section className={styles.listings}>
+    
+    <h1>Ogłoszenia</h1> 
+      <div className={styles.listings__group}> 
+        
+        {filteredListings.map((obj, index) => (
           <ListingItem
+          key={index}
           title={obj.title}
           content={obj.description}
-          // index={listingId}
-          image={obj.foto[0]}
+          index={obj.listingId}
+          image={obj.foto && obj.foto.length > 0 ? obj.foto[0] : '/src/assets/pictures/no-photo.webp'}
           />
         ))}
-    </section>
-    </>
-  )
-}
+      </div> 
+      
+  </section>)} 
+</>
+)}
 
 export default SearchPage
