85f0bbf1a8621b5979919c6d47235b76a166f7b2
Sebastian <sebastian.glonek86@icloud.com>
2024-06-28 17:09:27 +0200

    JFDZR13YT-13: Add categoryList to utils dir

---
+++ BarterNet/src/utils/categoriesList.js
@@ -0,0 +1,13 @@
+const categories=[
+    {category: "Dom i ogród", icon: "src/assets/icons/dom i ogród.png"},
+    {category: "Edukacja", icon: "src/assets/icons/edukacja.png"},
+    {category: "Transport i logistyka", icon: "src/assets/icons/transport i logistyka.png"},
+    {category: "Sport i rekreacja", icon: "src/assets/icons/sport i rekreacja.png"},
+    {category: "Hobby i rozrywka", icon: "src/assets/icons/hobby i rozrywka.png"},
+    {category: "Zwierzęta", icon: "src/assets/icons/zwierzęta.png"},
+    {category: "Technologia i IT", icon: "src/assets/icons/technologia i it.png"},
+    {category: "Zdrowie i uroda", icon: "src/assets/icons/zdrowie i uroda.png"},
+    {category: "Inne", icon: "src/assets/icons/inne.png"},
+]
+
+export default categories 
