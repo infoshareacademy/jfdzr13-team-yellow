fbf1c67eaaff92c4a8ddee87c665f6bbd1589f8b
Sebastian <sebastian.glonek86@icloud.com>
2024-06-30 22:24:24 +0200

    fix: Correct positioning in layout

--- BarterNet/src/components/Layout/Layout.jsx
+++ BarterNet/src/components/Layout/Layout.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import './Layout.css';
 
 const Layout = () => {
   return (
-    <div className="app-container">
+    <>
       <Header />
-      <main className="main-content">
+      <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
