be6aad289fa6659b10896242f1c8bbaddf66cc0e
Sylwia Konopacka-Bąk <sylwia.konopacka.bak@gmail.com>
2024-06-30 14:29:46 +0200

    fix UserMainPage

--- BarterNet/src/components/UserHomePage/UserHomePage.jsx
+++ BarterNet/src/components/UserHomePage/UserHomePage.jsx
@@ -7,30 +7,32 @@ import myProfileIcon from '../../assets/icons/myProfileIcon.png';
 
 const UserHomePage = () => {
   return (
-    <div className={styles.accountContainer}>
-      <div className={styles.tileContainer}>
-        <Link to="/addlisting" className={styles.accountTile}>
-          <img src={addListingIcon} alt="Add Listing" className={styles.icon} />
-          <span className={styles.tileLabel}>Dodaj ogłoszenie</span>
-          <span className={styles.tileDescription}>
-            W kilku prostych krokach dodawaj swoje ogłoszenia i dołącz do społeczności ludzi otwartych na wymianę. Poznaj korzyści barteru i stwórz własne ogłoszenie.
-          </span>
-        </Link>
-        <Link to="/searchpage" className={styles.accountTile}>
-          <img src={searchIcon} alt="Search" className={styles.icon} />
-          <span className={styles.tileLabel}>Wyszukaj ogłoszenie</span>
-          <span className={styles.tileDescription}>
-            Szukaj barterów blisko siebie i wyszukuj interesujące ogłoszenia z całej Polski. Dopasuj ogłoszenia do swoich potrzeb. Sprawdź interesujące bartery i wymieniaj się usługami. 
-          </span>
-        </Link>
-        <Link to="/myaccount" className={styles.accountTile}>
-          <img src={myProfileIcon} alt="My Profile" className={styles.icon} />
-          <span className={styles.tileLabel}>Twoje konto</span>
-          <span className={styles.tileDescription}>
-            Zobacz i edytuj swoje dane osobowe. Możesz aktualizować swoje
-            informacje kontaktowe, zmieniać hasło oraz ustawienia prywatności.
-          </span>
-        </Link>
+    <div className={styles.backgroundContainer}>
+      <div className={styles.accountContainer}>
+        <div className={styles.tileContainer}>
+          <Link to="/addlisting" className={styles.accountTile}>
+            <img src={addListingIcon} alt="Add Listing" className={styles.icon} />
+            <span className={styles.tileLabel}>Dodaj ogłoszenie</span>
+            <span className={styles.tileDescription}>
+              W kilku prostych krokach dodawaj swoje ogłoszenia i dołącz do społeczności ludzi otwartych na wymianę. Poznaj korzyści barteru i stwórz własne ogłoszenie.
+            </span>
+          </Link>
+          <Link to="/searchpage" className={styles.accountTile}>
+            <img src={searchIcon} alt="Search" className={styles.icon} />
+            <span className={styles.tileLabel}>Wyszukaj ogłoszenie</span>
+            <span className={styles.tileDescription}>
+              Szukaj barterów blisko siebie i wyszukuj interesujące ogłoszenia z całej Polski. Dopasuj ogłoszenia do swoich potrzeb. Sprawdź interesujące bartery i wymieniaj się usługami. 
+            </span>
+          </Link>
+          <Link to="/myaccount" className={styles.accountTile}>
+            <img src={myProfileIcon} alt="My Profile" className={styles.icon} />
+            <span className={styles.tileLabel}>Twoje konto</span>
+            <span className={styles.tileDescription}>
+              Zobacz i edytuj swoje dane osobowe. Możesz aktualizować swoje
+              informacje kontaktowe, zmieniać hasło oraz ustawienia prywatności.
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
   );
